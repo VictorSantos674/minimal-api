9d0a535c02f213717d382a7c824a732d22d828fc
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-09-15 22:21:39 +0000

    infra: tratamento global de erros com middleware customizado

---
+++ Api/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,28 @@
+using System.Net;
+using System.Text.Json;
+
+namespace MinimalApi.Middlewares;
+
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    public ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+            var result = JsonSerializer.Serialize(new { error = ex.Message });
+            await context.Response.WriteAsync(result);
+        }
+    }
+}
